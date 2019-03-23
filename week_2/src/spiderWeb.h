9d495619b1f0415a2e5efa711669c0802e58c761
PaulZhang2718 <48440045+PaulZhang2718@users.noreply.github.com>
2019-03-24 00:48:43 +0800

    Delete spiderWeb.h

--- week_2/src/spiderWeb.h
+++
@@ -1,7 +0,0 @@
-#ifndef SPIDER_WEB_
-#define SPIDER_WEB_
-#include <string>
-#include <cstdio>
-#include <iostream>
-int getUrl(const std::string &filename, const std::string &url); // download a url and store it by file
-#endif
