d367721ed55cdbbd45bcdc7bb59efd21bfb6485e
kevin <1847936171@qq.com>
2019-03-31 23:25:35 +0800

    update for improvement

--- week_3/2018202092FZJ/src/parse_html.cpp
+++ week_3/2018202092FZJ/src/parse_html.cpp
@@ -9,19 +9,20 @@ void Web_Crawler::find_next_url(){
 	std::string feature_string;
 	if (origin.find("econ") != origin.npos)
 		feature_string = "econ.ruc.edu.cn";
+	else feature_string = "ruc.edu.cn";
 	
 	while (url_begin != -1){
 		std::string new_url;
 		int url_end;
-		if(current_html[url_begin + 5] == '\"' || current_html[url_begin + 5] == '\''){
-	        url_end = current_html.find(current_html[url_begin + 5] == '\"' ? '\"' : '\'', url_begin + 6);
+		if(current_html[url_begin + 5] == '\'' || current_html[url_begin + 5] == '\"'){
+			url_end = current_html.find(current_html[url_begin + 5] == '\"' ? '\"' : '\'', url_begin + 6);
 			new_url = current_html.substr(url_begin + 6, url_end - url_begin - 6);
-		}
+		} 
 		else{
 			url_end = current_html.find('&', url_begin + 5);
 			new_url = current_html.substr(url_begin + 5, url_end - url_begin - 5);
 		}
-		
+
 		url_begin = current_html.find("href=", url_end);
 		
 		if (new_url.find(feature_string) != new_url.npos){
@@ -29,7 +30,8 @@ void Web_Crawler::find_next_url(){
 				new_url.erase(0,7);
 		}
 		else if (new_url.find("http:") == new_url.npos){
-				if (new_url[0] == '/') new_url = current_url.substr(0, current_url.find_last_of('/') + 1) + new_url.substr(1);
+				if (new_url[0] == '/')
+					new_url = current_url.substr(0, current_url.find_last_of('/') + 1) + new_url.substr(1);
 				else new_url = current_url.substr(0, current_url.find_last_of('/') + 1) + new_url;
 			}
 			
