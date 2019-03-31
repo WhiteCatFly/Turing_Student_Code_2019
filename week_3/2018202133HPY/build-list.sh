cf32e83da8d2492f577b53651b206f8a12983776
ceerRep <ceerrep@gmail.com>
2019-03-31 12:30:58 +0800

    cR's homework

---
+++ week_3/2018202133HPY/build-list.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+SRCS=`find src/ -regex ".*\.cpp"`
+OBJS=
+DEPENDS=
+
+for src in $SRCS 
+do
+	obj=tmp/`basename $src .cpp`.o
+	depend=tmp/`basename $src .cpp`.d
+	
+	OBJS="$OBJS $obj"
+	DEPENDS="$DEPENDS $depend"
+
+	#echo $obj
+	#echo $depend
+	#'\t$$(CXX) $$(CXXFLAGS) -c $< -o ${patsubst src/%.cpp, tmp/%.o, $<}'
+
+	echo -e "$depend: $src" 
+	echo -e "\t\$(CXX) -MM $src | sed 's,^,tmp/,' > $depend"
+	echo -e "\techo -e '\t\$\$(CXX) \$\$(CXXFLAGS) -c $src -o $obj' >> $depend"
+	echo
+done
+
+echo "OBJ=$OBJS"
+echo "DEPEND=$DEPENDS"
