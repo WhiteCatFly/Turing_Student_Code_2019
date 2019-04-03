858a66a11e870ea8d573a04913de746e5d46a15f
HYX <1424670564@qq.com>
2019-04-03 18:52:16 +0800

    week3 homework

--- week_3/2018202118HYX/src/main.cpp
+++ week_3/2018202118HYX/src/main.cpp
@@ -1,34 +1,19 @@
-#include"head.h"
+#include "head.h"
+#include<ctime>
+using namespace std;
+
 
 int main()
 {
-    string root="http://econ.ruc.edu.cn/";
-    int total=0;
+    clock_t start,end;
+    start=clock();
+
+    string root="econ.ruc.edu.cn";
+    crawl run(root);
+    run.BFS();
     
-    queue<string> todo;
-    set<string> done;
-    set<string>urls;
+    end=clock();
+    cout<<double(end-start)/CLOCKS_PER_SEC;
     
-    string current_url;
-    todo.push(root);
-
-    while(!todo.empty() )
-    {
-        current_url=todo.front();
-        todo.pop();
-        done.insert(current_url);
-
-        parse_url s(current_url,root);
-        s.get_content(total);
-        urls.clear();
-        s.parse(urls);
-
-        for(auto url:urls)
-        {
-            if(done.find(url)==done.end())
-                todo.push(url);
-        }
-    }
-    cerr<<"total:"<<total<<endl;
     return 0;
 }
