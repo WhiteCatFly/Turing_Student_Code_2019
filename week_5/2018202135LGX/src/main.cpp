5343f8519ff2a839ac51846b928ef18fc24b29c1
Guoxiang Li <48539371+driftee@users.noreply.github.com>
2019-04-14 15:40:19 +0800

    Update main.cpp

--- week_5/2018202135LGX/src/main.cpp
+++ week_5/2018202135LGX/src/main.cpp
@@ -1,11 +1,19 @@
+// example main
+
+
 #include"BigInteger.h"
 
 int main(){
-    BigInteger a;
-    BigInteger b(1);
-    int m = 344243243;
-    a = m + b;
-    std::cout << a;
+    BigInteger a; //default a = 0
+    BigInteger b(13422433); // initialize b with int
+    BigInteger c("201820213520182021352018202135"); // initialize c with const char*
+    std::string m = "1433223";
+    BigInteger d = m; // initialize d with std::string
+    BigInteger e = d; // initialize e with another BigInteger
+    a = b + c; // BigInteger plus BigInteger
+    BigInteger d_1 = d + 1327; // BigInteger plus int
+    BigInteger d_2 = 1327 + d; // int plus BigInteger
+    std::cout << d_1 << " " << d_2 << std::endl; // << overloading
 }
 
 
