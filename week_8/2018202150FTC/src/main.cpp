5f21a556f79279ead8d71a8c382697954a335228
Lucasftc <1607228797@qq.com>
2019-05-25 01:33:41 -0700

    8th_homework

---
+++ week_8/2018202150FTC/src/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include"search.h"
+extern void search(std::string,int);
+int main()
+{
+    
+    std::string query="";
+    Searchengine test=Searchengine(query,10);
+}
